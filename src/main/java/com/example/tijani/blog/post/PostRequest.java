884d787919e84383882c2fffe65f1f918e774a97
moyosore1 <moyosoreolumideobi@gmail.com>
2022-04-20 10:01:52 -0400

    Update PostRequest.java

--- src/main/java/com/example/tijani/blog/post/PostRequest.java
+++ src/main/java/com/example/tijani/blog/post/PostRequest.java
@@ -1,6 +1,8 @@
 package com.example.tijani.blog.post;
 
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -11,7 +13,11 @@ import lombok.ToString;
 @EqualsAndHashCode
 @ToString
 public class PostRequest {
+
+  @Size(min = 5, max = 255)
   private String title;
+  @NotBlank(message = "Content shall be required to create post.")
   private String content;
+  @NotBlank(message = "Set category id.")
   private int category;
 }
